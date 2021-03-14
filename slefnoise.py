bfc3db193b3d448bf11e9059c6ec2575bfccecef
ZenoMilk12 <xalexclashroyale@gmail.com>
2021-03-14 20:09:39 +0200

    making .env file

--- slefnoise.py
+++ slefnoise.py
@@ -1,4 +1,10 @@
 import discord
 from discord.ext import commands
 import sys
+import os
 
+token = os.getenv('token')
+
+prefix = os.getenv('prefix')
+
+selfnoise = commands.Bot(command_prefix=prefix)
