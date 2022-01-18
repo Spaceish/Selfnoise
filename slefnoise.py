d38e397ffa5985b1b60ec766cf88bb958e5aa6ba
ZenoMilk12 <xalexclashroyale@gmail.com>
2022-01-18 23:10:54 +0200

    presence

--- slefnoise.py
+++ slefnoise.py
@@ -1,16 +1,20 @@
 import discord
 from discord.ext import commands
-import sys
 import os
 
 token = os.getenv('token')
 
 prefix = os.getenv('prefix')
 
-selfnoise = commands.Bot(command_prefix=prefix)
+selfnoise = commands.Bot(command_prefix=prefix, self_bot=True)
 
 @selfnoise.event
 async def on_ready():
-    
+    print('Logged in as')
+    print(selfnoise.user.name)
+    print(selfnoise.user.id)
+    print('[------]')
+    # change status to "Toate ca toate"
+    await selfnoise.change_presence(game=discord.Game(name='Toate ca toate')) 
 
 selfnoise.run(token)
